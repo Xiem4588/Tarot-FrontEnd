a4e340f0296c8239f88b2669a31a635914be5392
Xiem4588 <xiem.bg@gmail.com>
2023-08-25 10:45:29 +0700

    update fonts for ios & android

--- react-native.config.js
+++ react-native.config.js
@@ -1,12 +1,9 @@
 //react-native.config.js
 
 module.exports = {
-  resolver: {
-    assetExts: ['ttf', 'png', 'jpg', 'jpeg', 'gif'], // Thêm các định dạng file asset bạn muốn hỗ trợ
-  },
   project: {
     ios: {},
     android: {},
   },
-  assets: ['src/assets/fonts'],
+  assets: ['./assets/fonts'],
 };
